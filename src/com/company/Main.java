a0de936c7d3654ef228cb29ff6a1a2942172b537
meder9608 <meder9608@gmail.com>
2019-02-19 20:04:43 +0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String variable= "Hello, world";
+        System.out.printlnvariable);
+    }
+}
+
+// git add .
