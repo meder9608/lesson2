e64e62bec9d73a174c6c036186f19ac99c3bd572
meder9608 <meder9608@gmail.com>
2019-02-22 03:42:51 +0600

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
         String variable= "Hello, world";
-        System.out.printlnvariable);
+        System.out.println(variable);
     }
 }
 
 // git add .
+// git status
+
+//git commit -m "first commit"
+
+// git config --global user.email meder9608@gmail.com
+
+// git config --global user.name "meder9608"
+
+//git pull
+
+//git push -u origin master
+
+// git checkout -b branch2 - создание новой ветки
+
+// git checkout master - переключение на ветку
+
+// git merge branch2 - слияние ветки в текущую
+
+// git log
